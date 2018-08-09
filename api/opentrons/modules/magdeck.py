f3bb3104471f365a35a12c2efb7f776d0913c839
Sanniti <sanniti.p@nyu.edu>
2018-08-09 15:20:48 -0400

    added route and final update step. fails tests

--- api/opentrons/modules/magdeck.py
+++ api/opentrons/modules/magdeck.py
@@ -160,6 +160,7 @@ class MagDeck:
             log.debug('Firmware uploaded successfully')
         else:
             log.debug('Firmware upload failed\n{}'.format(res.strip()))
+        return res.strip()
 
 
 def discover_ports():
